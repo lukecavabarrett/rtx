596b794acba49a627b0dc20b212930b4dd20e686
lukecavabarrett <luca.cavallery@gmail.com>
2019-06-11 15:52:26 +0100

    Initial commit

---
+++ rtx.h
@@ -0,0 +1,15 @@
+#ifndef _INCLUDE_RTX_H_
+#define _INCLUDE_RTX_H_
+
+#include "typedefs.h"
+
+namespace rtx{
+
+//forward declarate all classes
+
+};
+
+//include all .h files
+
+
+#endif
---
+++ typedefs.h
@@ -0,0 +1,13 @@
+#ifndef _INCLUDE_TYPEDEFS_H_
+#define _INCLUDE_TYPEDEFS_H_
+
+#include <bits/stdint-uintn.h>
+
+namespace gfx{
+    
+typedef double dtype;
+typedef uint32_t utype;
+typedef int stype;
+
+};
+#endif
