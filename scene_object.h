dc83e99c571d0c69efb35c6b5c390f8ad5fa5fd7
lukecavabarrett <luca.cavallery@gmail.com>
2019-06-12 19:22:00 +0100

    Scene improved

--- scene_object.h
+++ scene_object.h
@@ -6,6 +6,7 @@
 #include "vector3.h"
 #include "color_rgb.h"
 #include "ray3.h"
+#include "scene.h"
 
 namespace rtx{
     class scene_object{
@@ -14,7 +15,7 @@ namespace rtx{
     virtual color_rgb get_color_at(const vector3& v) const;
     virtual vector3 get_normal_at(const vector3& v) const = 0;
     virtual vector3 get_mapping_at(const vector3& v) const;
-    virtual std::pair<bool,vector3> intersection_with(const ray3& r) const = 0;
+    virtual std::pair<dtype,vector3> intersection_with(const ray3& r) const = 0;
     virtual color_rgb trace(const vector3& p,const ray3& r,const scene& s,int bounces=0) const = 0;
     };
 };
